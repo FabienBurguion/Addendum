8955c2a5140635ae8816bc1a142fe23fc4c263c8
Fabien Burguion <fabien.burguion@epita.fr>
2025-06-10 15:44:23 +0200

    funny

--- AdventOfCode19/src/main/java/org/epita/models/ResourceType.java
+++ AdventOfCode19/src/main/java/org/epita/models/ResourceType.java
@@ -1,5 +1,9 @@
 package org.epita.models;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResourceType {
     public static final int ORE = 0;
     public static final int CLAY = 1;
@@ -8,6 +12,19 @@ public class ResourceType {
     public static final int DIAMOND = 4;
 
     public static int getLastResourceType() {
-        return DIAMOND;
+        try {
+            Field[] fields = ResourceType.class.getDeclaredFields();
+            List<Integer> values = new ArrayList<>();
+
+            for (Field field : fields) {
+                if (field.getType() == int.class && java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+                    values.add((int) field.get(null));
+                }
+            }
+
+            return values.stream().max(Integer::compare).orElse(0);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Failed to get last resource type", e);
+        }
     }
 }
